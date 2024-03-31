5fc2271fff90fa17e58bec3954da364a43f92733
wa-ko <wai.create2021@gmail.com>
2024-03-31 16:02:24 +0900

    first commit

---
+++ meeTomo/meeTomoApp.swift
@@ -0,0 +1,17 @@
+//
+//  meeTomoApp.swift
+//  meeTomo
+//
+//  Created by 若生優希 on 2024/03/31.
+//
+
+import SwiftUI
+
+@main
+struct meeTomoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
