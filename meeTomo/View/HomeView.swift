fd85fb8e1e42b3ad04b9cf8a77141dc994c3b64b
satyappp <s1282006@u-aizu.ac.jp>
2024-05-17 01:12:12 +0900

    Refactor HomeView

--- meeTomo/View/HomeView.swift
+++ meeTomo/View/HomeView.swift
@@ -2,19 +2,17 @@
 //  HomeView.swift
 //  meeTomo
 //
-//  Created by 若生優希 on 2024/04/09.
+//  Created by Satya on 2024/05/17.
 //
 
 import SwiftUI
 import SwiftData
 
 struct HomeView: View {
-    //SwiftData
     @Environment(\.modelContext) private var context
     @Query private var friends: [Friend]
     @State var isShowAdd = false
     @State private var isShowSetting = false
-    //カメラ使用
     @State private var isPresentedCameraView = false
     @State private var image: UIImage?
     @State private var currentIndex = 0
@@ -33,142 +31,154 @@ struct HomeView: View {
                         .transition(.move(edge: .bottom))
                 } else {
                     VStack {
-                        HStack{
-                            Button(action: {
-                                isShowSetting.toggle()
-                            }, label: {
-                                Image(systemName: "gearshape")
-                                    .foregroundColor(.gray)
-                                    .font(.title3)
-                            })
-                            .sheet(isPresented: $isShowSetting, content: {
-                                SettingView()
-                            })
-                            Spacer()
-                            VStack{
-                                if !friends.isEmpty {
-                                    Text(friends[currentIndex % friends.count].name)
-                                        .foregroundColor(.gray)
-                                } else {
-                                    Text("友達がいません")
-                                        .foregroundColor(.gray)
-                                }
-                            }
-                            .foregroundColor(.gray)
-                            Spacer()
-                            Button(action: {
-                                isShowAdd.toggle()
-                            }, label: {
-                                Image(systemName: "plus")
-                                    .foregroundColor(.gray)
-                                    .font(.title3)
-                            })
-                            .sheet(isPresented: $isShowAdd, content: {
-                                AddFriendView(isShowAdd: $isShowAdd)
-                            })
-                        }
-                        .padding()
+                        topBar
                         Spacer()
-                        ZStack {
-                            PolaroidView(image: image, rotationDegrees: 4, destination: nil, width: 300, height: 500, namespace: animationNamespace, id: UUID())
-                                .onTapGesture {
-                                    withAnimation {
-                                        showGallery.toggle()
-                                    }
-                                }
-                            PolaroidView(image: image, rotationDegrees: -6, destination: nil, width: 300, height: 500, namespace: animationNamespace, id: UUID())
-                                .onTapGesture {
-                                    withAnimation {
-                                        showGallery.toggle()
-                                    }
-                                }
-                            PolaroidView(image: image, rotationDegrees: 0, destination: nil, width: 300, height: 500, namespace: animationNamespace, id: UUID())
-                                .onTapGesture {
-                                    withAnimation {
-                                        showGallery.toggle()
-                                    }
-                                }
-                            if !friends.isEmpty {
-                                if let latestPhoto = friends[currentIndex % friends.count].latestPhoto(), let image = UIImage(data: latestPhoto.image) {
-                                    PolaroidView(image: image, rotationDegrees: 0, destination: nil, width: 300, height: 500, namespace: animationNamespace, id: UUID())
-                                        .onTapGesture {
-                                            withAnimation {
-                                                showGallery.toggle()
-                                            }
-                                        }
-                                } else {
-                                    Text("No photos available")
-                                }
-                            } else {
-                                Text("友達がいません")
-                            }
-                        }
+                        photoStack
                         Spacer()
-                        HStack{
-                            Menu{
-                                Button{
-                                } label: {
-                                    Label("1つ後ろに", systemImage: "rectangle.stack.badge.plus")
-                                }
-                                Button{
-                                } label: {
-                                    Label("半分後ろに", systemImage: "folder.badge.plus")
-                                }
-                                Button{
-                                } label: {
-                                    Label("1番後ろに", systemImage: "rectangle.stack.badge.person.crop")
-                                }
-                            } label: {
-                                Image(systemName: "return")
-                                    .foregroundColor(.gray)
-                                    .font(.title3)
-                            }
-                            Spacer()
-                            VStack {
-                                if !friends.isEmpty {
-                                    Button {
-                                        currentIndex = (currentIndex + 1) % (friends.count)
-                                    } label: {
-                                        Text("next →")
-                                            .foregroundColor(.gray)
-                                            .font(.title3)
-                                    }
-                                    Text(friends.isEmpty ? "友達がいません" : "\(friends[(currentIndex + 1) % friends.count].name)")
-                                } else {
-                                    Text("next →")
-                                        .foregroundColor(.gray)
-                                        .font(.title3)
-                                    Text("友達がいません")
-                                }
-                            }
-                            .foregroundColor(.gray)
-                            Spacer()
-                            VStack {
-                                Button {
-                                    isPresentedCameraView = true
-                                } label: {
-                                    Image(systemName: "camera")
-                                        .foregroundColor(.gray)
-                                        .font(.title3)
-                                }
-                                if let image {
-                                    Image(uiImage: image)
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width: 300)
-                                }
-                            }
-                            .fullScreenCover(isPresented: $isPresentedCameraView) {
-                                CameraView(image: $image).ignoresSafeArea()
-                            }
-                        }
-                        .padding()
+                        bottomBar
                     }
                     .padding()
                     .transition(.move(edge: .top))
                 }
             }
+            .background(
+                LinearGradient(gradient: Gradient(colors: [.white, .black]), startPoint: .top, endPoint: .bottom)
+            )
         }
+    }
+    
+    private var topBar: some View {
+        HStack {
+            Button(action: {
+                isShowSetting.toggle()
+            }) {
+                Image(systemName: "gearshape")
+                    .foregroundColor(.gray)
+                    .font(.title3)
+            }
+            .sheet(isPresented: $isShowSetting) {
+                SettingView()
+            }
+            Spacer()
+            VStack {
+                if !friends.isEmpty {
+                    Text(friends[currentIndex % friends.count].name)
+                        .foregroundColor(.gray)
+                } else {
+                    Text("友達がいません")
+                        .foregroundColor(.gray)
+                }
+            }
+            .foregroundColor(.gray)
+            Spacer()
+            Button(action: {
+                isShowAdd.toggle()
+            }) {
+                Image(systemName: "plus")
+                    .foregroundColor(.gray)
+                    .font(.title3)
+            }
+            .sheet(isPresented: $isShowAdd) {
+                AddFriendView(isShowAdd: $isShowAdd)
+            }
+        }
+        .padding()
+    }
+    
+    private var photoStack: some View {
+        ZStack {
+            PolaroidView(image: image, rotationDegrees: 4, destination: nil, width: 300, height: 500, namespace: animationNamespace, id: UUID())
+                .onTapGesture {
+                    withAnimation {
+                        showGallery.toggle()
+                    }
+                }
+            PolaroidView(image: image, rotationDegrees: -6, destination: nil, width: 300, height: 500, namespace: animationNamespace, id: UUID())
+                .onTapGesture {
+                    withAnimation {
+                        showGallery.toggle()
+                    }
+                }
+            PolaroidView(image: image, rotationDegrees: 0, destination: nil, width: 300, height: 500, namespace: animationNamespace, id: UUID())
+                .onTapGesture {
+                    withAnimation {
+                        showGallery.toggle()
+                    }
+                }
+            if !friends.isEmpty {
+                if let latestPhoto = friends[currentIndex % friends.count].latestPhoto(), let image = UIImage(data: latestPhoto.image) {
+                    PolaroidView(image: image, rotationDegrees: 0, destination: nil, width: 300, height: 500, namespace: animationNamespace, id: UUID())
+                        .onTapGesture {
+                            withAnimation {
+                                showGallery.toggle()
+                            }
+                        }
+                } else {
+                    Text("No photos available")
+                }
+            } else {
+                Text("友達がいません")
+            }
+        }
+    }
+    
+    private var bottomBar: some View {
+        HStack {
+            Menu {
+                Button {} label: {
+                    Label("1つ後ろに", systemImage: "rectangle.stack.badge.plus")
+                }
+                Button {} label: {
+                    Label("半分後ろに", systemImage: "folder.badge.plus")
+                }
+                Button {} label: {
+                    Label("1番後ろに", systemImage: "rectangle.stack.badge.person.crop")
+                }
+            } label: {
+                Image(systemName: "return")
+                    .foregroundColor(.gray)
+                    .font(.title3)
+            }
+            Spacer()
+            VStack {
+                if !friends.isEmpty {
+                    Button {
+                        currentIndex = (currentIndex + 1) % friends.count
+                    } label: {
+                        Text("next →")
+                            .foregroundColor(.gray)
+                            .font(.title3)
+                    }
+                    Text(friends.isEmpty ? "友達がいません" : "\(friends[(currentIndex + 1) % friends.count].name)")
+                } else {
+                    Text("next →")
+                        .foregroundColor(.gray)
+                        .font(.title3)
+                    Text("友達がいません")
+                }
+            }
+            .foregroundColor(.gray)
+            Spacer()
+            VStack {
+                Button {
+                    isPresentedCameraView = true
+                } label: {
+                    Image(systemName: "camera")
+                        .foregroundColor(.gray)
+                        .font(.title3)
+                }
+                if let image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 300)
+                }
+            }
+            .fullScreenCover(isPresented: $isPresentedCameraView) {
+                CameraView(image: $image).ignoresSafeArea()
+            }
+        }
+        .padding()
     }
 }
 
