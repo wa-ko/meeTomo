4e9b77e4db412b4a9ff0c286ba3746ab5f03f50c
satyappp <s1282006@u-aizu.ac.jp>
2024-05-16 23:49:02 +0900

    Added polaroid view

---
+++ meeTomo/View/GalleryView.swift
@@ -0,0 +1,18 @@
+//
+//  GalleryView.swift
+//  meeTomo
+//
+//  Created by Apple on 2024/05/16.
+//
+
+import SwiftUI
+
+struct GalleryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GalleryView()
+}
